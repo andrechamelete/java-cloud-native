9ed45fe9b786635c2ccb47f65618b46f5e535cf8
andrechamelete <chamelete@gmail.com>
2025-03-31 15:46:51 -0300

    desafio

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -15,6 +15,8 @@ public class Contador {
         } catch (ParametrosInvalidosException e) {
             System.out.println("O segundo parâmetro deve ser maior que o primeiro");            
         }
+
+        terminal.close();
     }
 
     static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
