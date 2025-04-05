146421027091969075c55fec3c3a0e32daa2fff4
André Chamelete <chamelete@gmail.com>
2025-04-05 20:20:27 -0300

    projeto Sudoku

---
+++ Sudoku/src/sudoku/UIMain.java
@@ -0,0 +1,18 @@
+package sudoku;
+
+import sudoku.ui.custom.screen.MainScreen;
+
+import static java.util.stream.Collectors.toMap;
+import java.util.stream.Stream;
+
+public class UIMain {
+    public static void main(String[] args) {
+        final var gameConfig = Stream.of(args).collect(toMap(
+            k -> k.split(":")[0],
+            v -> v.split(";")[1]
+            ));
+        
+        var mainScreen = new MainScreen(gameConfig);
+        mainScreen.buildMainScreen();;
+    }
+}
