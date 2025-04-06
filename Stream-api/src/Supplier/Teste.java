d419681e045dc75199288c2c2f13ca69eb4ec1c1
André Chamelete <chamelete@gmail.com>
2025-04-06 10:49:02 -0300

    ajuste

---
+++ Stream-api/src/Supplier/Teste.java
@@ -0,0 +1,19 @@
+package Supplier;
+class Teste {
+    public static void main(String[] args) {
+        int[] lista = {3,4,5,6,7};
+        
+        int cont = 0;
+
+        	
+        while(cont < 5){
+            System.out.println(lista[cont]);
+            cont = cont + 1;
+        }
+
+        	
+        for(cont=0; cont<5; cont++) {
+            System.out.println(lista[cont]);
+        }
+    }
+}
